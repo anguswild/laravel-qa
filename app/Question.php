79b08f371b1d57bdafac1ef51efe83ed5c31348d
Patricio Quezada AsusK501 <patricio.quezada05@gmail.com>
2019-10-29 20:57:38 -0300

    Factories

--- app/Question.php
+++ app/Question.php
@@ -7,7 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     protected $fillable = ['title', 'body'];
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo('App\User');
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
     }
 }
